9c13dba848e7559f8584ec9bd51b407659525406
LONGJOHN DAGOGO <bubobuabo@gmail>
2022-09-26 17:55:40 -0400

    gjjk

---
+++ src/AddressBook.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private ArrayList<BuddyInfo> myBuddies;
+    public AddressBook(){
+        myBuddies = new ArrayList<>();
+    }
+
+    public void addBuddy(BuddyInfo aBuddy){
+        if (aBuddy != null){
+            myBuddies.add(aBuddy);
+        }
+    }
+
+    public BuddyInfo removeBuddy(int index){
+        if (index >= 0 && index < myBuddies.size()){
+            return myBuddies.remove(index);
+        }
+        return null;
+    }
+
+    public static void main(String[] args){
+        BuddyInfo buddy = new BuddyInfo("john","Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
+    }
+
+}
